9f3c9a97e5acffd1c61415ce631b5dc4c04bf491
mayli4 <sydsleeper@gmail.com>
2025-10-28 22:09:57 -0400

    feat: initial sdl context creation

---
+++ src/StainedGlass/SdlCtx.cs
@@ -0,0 +1,54 @@
+﻿using SDL3;
+using System.Diagnostics;
+
+namespace StainedGlass;
+
+/// <summary>
+///     Contains info related to the SDL library.
+/// </summary>
+public sealed unsafe class SdlCtx : IDisposable {
+    public readonly int Version;
+    private readonly string _platform;
+    
+    public ReadOnlySpan<char> Platform => _platform.AsSpan();
+
+    public SdlCtx(SDL.InitFlags flags) {
+        var init = SDL.Init(flags);
+        if(!init) {
+            throw new Exception($"sdl init failed. {SDL.GetError()}");
+        }
+
+        Version = SDL.GetVersion();
+        _platform = SDL.GetPlatform();
+        SDL.SetLogOutputFunction((_, category, priority, message) => {
+#if DEBUG
+            if (priority >= SDL.LogPriority.Error) {
+                throw new Exception($"{priority} [{category}]: {message}");
+            }
+#endif
+            
+            Console.WriteLine($"{priority} [{category}]: {message}");
+        }, 0);
+    }
+    
+    public bool PollEvent(out SDL.Event message) {
+        var poll = SDL.PollEvent(out var evt);
+        message = evt;
+        return poll;
+    }
+
+    public string[] GetVulkanExtensions() {
+        var extensionNames = SDL.VulkanGetInstanceExtensions(out _);
+        Debug.Assert(extensionNames != null);
+        
+        for (int i = 0; i < extensionNames.Length; i++) {
+            extensionNames[i] = extensionNames[i];
+        }
+
+        return extensionNames.ToArray();
+    }
+
+    public void Dispose() {
+        SDL.Quit();
+    }
+}
---
+++ src/StainedGlass/SdlWindow.cs
@@ -0,0 +1,45 @@
+﻿using SDL3;
+using System.Runtime.CompilerServices;
+
+namespace StainedGlass;
+
+/// <summary>
+///     A basic wrapped around an SDL window. Up to the user to implement windowing functions not covered here.
+/// </summary>
+public unsafe struct SdlWindow : IDisposable{
+    private readonly IntPtr _window;
+    private readonly nint _surface;
+
+    public readonly IntPtr Surface {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        get => _surface;
+    }
+
+    public SdlWindow(int width, int height, SDL.WindowFlags flags) {
+        _window = SDL.CreateWindow("asd", width, height, flags);
+    }
+
+    public readonly uint Id => SDL.GetWindowID(_window);
+    
+    public readonly bool IsDisposed => _window == IntPtr.Zero;
+    public readonly SDL.WindowFlags Flags => SDL.GetWindowFlags(_window);
+    
+    public ReadOnlySpan<char> Title {
+        get => SDL.GetWindowTitle(_window);
+        set => SDL.SetWindowTitle(_window, value.ToString());
+    }
+    
+    public nint CreateVulkanSurface(nint vulkanInstance) {
+        nint allocator = 0;
+        bool success = SDL.VulkanCreateSurface(_window, vulkanInstance, allocator, out var surfacePointer);
+        if (!success) {
+            throw new Exception($"could not create vk surface: {SDL.GetError()}");
+        }
+
+        return new(surfacePointer);
+    }
+
+    public void Dispose() {
+        SDL.DestroyWindow(_window);
+    }
+}
