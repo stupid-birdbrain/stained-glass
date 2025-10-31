7fe7e92533f0608effab63c5f9da43a08a329bd4
mayli4 <sydsleeper@gmail.com>
2025-10-31 02:35:12 -0400

    feat: sdl display

--- src/StainedGlass/SdlDisplay.cs
+++ src/StainedGlass/SdlDisplay.cs
@@ -2,11 +2,11 @@
 
 namespace StainedGlass;
 
-public readonly unsafe struct SdlDisplay(SDL.DisplayMode* displayMode) {
-    private readonly SDL.DisplayMode* _displayMode = displayMode;
+public readonly unsafe struct SdlDisplay(SDL.DisplayMode displayMode) {
+    private readonly SDL.DisplayMode _displayMode = displayMode;
 
-    public uint Id => _displayMode->DisplayID;
-    public uint Width => (uint)_displayMode->W;
-    public uint Height => (uint)_displayMode->H;
-    public uint RefreshRate => (uint)_displayMode->RefreshRate;
+    public uint Id => _displayMode.DisplayID;
+    public uint Width => (uint)_displayMode.W;
+    public uint Height => (uint)_displayMode.H;
+    public uint RefreshRate => (uint)_displayMode.RefreshRate;
 }
--- src/StainedGlass/SdlWindow.cs
+++ src/StainedGlass/SdlWindow.cs
@@ -1,28 +1,32 @@
 ﻿using SDL3;
-using System.Runtime.CompilerServices;
 
 namespace StainedGlass;
 
 /// <summary>
-///     A basic wrapped around an SDL window. Up to the user to implement windowing functions not covered here.
+///     A basic wrapper around an SDL window. Up to the user to implement windowing functions not covered here.
 /// </summary>
-public unsafe struct SdlWindow : IDisposable{
+public unsafe struct SdlWindow : IDisposable {
     private readonly IntPtr _window;
-    private readonly nint _surface;
 
-    public readonly IntPtr Surface {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        get => _surface;
-    }
-
-    public SdlWindow(int width, int height, SDL.WindowFlags flags) {
-        _window = SDL.CreateWindow("asd", width, height, flags);
+    internal SdlWindow(IntPtr window) { _window = window; }
+    
+    public SdlWindow(ReadOnlySpan<char> title, int width, int height, SDL.WindowFlags flags) {
+        _window = SDL.CreateWindow(title.ToString(), width, height, flags);
     }
 
     public readonly uint Id => SDL.GetWindowID(_window);
     
     public readonly bool IsDisposed => _window == IntPtr.Zero;
     public readonly SDL.WindowFlags Flags => SDL.GetWindowFlags(_window);
+
+    public readonly SdlDisplay Display {
+        get {
+            var id = SDL.GetDisplayForWindow(_window);
+            SDL.DisplayMode? mode = SDL.GetCurrentDisplayMode(id);
+
+            return new SdlDisplay((SDL.DisplayMode)mode!);
+        }
+    }
     
     public ReadOnlySpan<char> Title {
         get => SDL.GetWindowTitle(_window);
@@ -35,8 +39,21 @@ public unsafe struct SdlWindow : IDisposable{
         if (!success) {
             throw new Exception($"could not create vk surface: {SDL.GetError()}");
         }
-
+        
         return new(surfacePointer);
+    }
+
+    public readonly void SetTransparency(float alpha) {
+        var format = SDL.GetWindowPixelFormat(_window);
+        SDL.GetWindowSize(_window, out var width, out var height);
+        var shape = SDL.CreateSurface(width, height, format);
+        SDL.ClearSurface(_window, 0, 0, 0, alpha);
+        var success = SDL.SetWindowShape(_window, shape);
+        if (!success) {
+            throw new Exception($"could not set window shape: {SDL.GetError()}");
+        }
+        
+        SDL.DestroySurface(shape);
     }
 
     public void Dispose() {
