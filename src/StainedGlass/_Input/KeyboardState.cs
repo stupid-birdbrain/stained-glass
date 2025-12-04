8bc25b0b29f503fa2db0f5f3d420ecd1a261c77e
mayli4 <sydsleeper@gmail.com>
2025-12-04 17:08:10 -0500

    wip: keyboard input

---
+++ src/StainedGlass/_Input/KeyboardState.cs
@@ -0,0 +1,70 @@
+﻿using SDL3;
+
+namespace StainedGlass;
+
+public struct KeyboardState {
+    private const int key_buffer_size = 16;
+
+    private KeyBuffer _currentKeys;
+    private KeyBuffer _previousKeys;
+    
+    public bool IsKeyDown(SDL.Scancode scancode) => _currentKeys.IsKeyDown(scancode);
+    
+    public bool IsKeyUp(SDL.Scancode scancode) => !_currentKeys.IsKeyDown(scancode);
+    
+    public bool WasKeyPressed(SDL.Scancode scancode) {
+        bool wasDownPrev = _previousKeys.IsKeyDown(scancode);
+        bool isDownCurrent = _currentKeys.IsKeyDown(scancode);
+
+        return isDownCurrent && !wasDownPrev;
+    }
+
+    public void SetKeyState(SDL.Scancode scancode, bool isDown) {
+        _currentKeys.SetKeyDown(scancode, isDown);
+    }
+
+    public void Update() {
+        _previousKeys.CopyFrom(_currentKeys);
+    }
+
+    internal void Clear() {
+        _currentKeys.Clear();
+        _previousKeys.Clear();
+    }
+    
+    private unsafe struct KeyBuffer {
+        private fixed ulong _buffer[key_buffer_size];
+
+        public bool IsKeyDown(SDL.Scancode scancode) {
+            int index = (int)scancode / 64;
+            int bit = (int)scancode % 64;
+            if (index < 0 || index >= key_buffer_size) return false;
+            return (_buffer[index] & (1UL << bit)) != 0;
+        }
+
+        public void SetKeyDown(SDL.Scancode scancode, bool down) {
+            int index = (int)scancode / 64;
+            int bit = (int)scancode % 64;
+            if (index < 0 || index >= key_buffer_size) return; 
+
+            if (down) {
+                _buffer[index] |= (1UL << bit);
+            }
+            else {
+                _buffer[index] &= ~(1UL << bit);
+            }
+        }
+
+        public void Clear() {
+            for (int i = 0; i < key_buffer_size; i++) {
+                _buffer[i] = 0;
+            }
+        }
+
+        public void CopyFrom(KeyBuffer source) {
+            for (int i = 0; i < key_buffer_size; i++) {
+                _buffer[i] = source._buffer[i];
+            }
+        }
+    }
+}
