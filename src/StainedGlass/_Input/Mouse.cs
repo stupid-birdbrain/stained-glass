4321040befcbe5e11434c4544d28f3c45d1604ea
mayli4 <sydsleeper@gmail.com>
2025-12-04 20:51:11 -0500

    feat: mouse inputs

---
+++ src/StainedGlass/_Input/Mouse.cs
@@ -0,0 +1,59 @@
+﻿using System.Numerics;
+
+namespace StainedGlass;
+
+public struct Mouse {
+    public enum Button : byte {
+        LeftButton = 1,
+        MiddleButton = 2,
+        RightButton = 3,
+    }
+    
+    private MouseState _currentState;
+    private MouseState _previousState;
+    
+    public Vector2 GetPosition() => _currentState.Position;
+    public Vector2 GetScrollDelta() => _currentState.Scroll;
+    public Vector2 GetRelativeDelta() => _currentState.RelativeDelta;
+    
+    public bool IsButtonDown(Button button) => _currentState.IsButtonDown(button);
+    public bool IsButtonUp(Button button) => !_currentState.IsButtonDown(button);
+
+    public bool WasButtonPressed(Button button) {
+        bool wasDownPrev = _previousState.IsButtonDown(button);
+        bool isDownCurrent = _currentState.IsButtonDown(button);
+        return isDownCurrent && !wasDownPrev;
+    }
+
+    public bool WasButtonReleased(Button button) {
+        bool wasDownPrev = _previousState.IsButtonDown(button);
+        bool isDownCurrent = _currentState.IsButtonDown(button);
+        return !isDownCurrent && wasDownPrev;
+    }
+    
+    public void Update() {
+        _previousState.CopyFrom(_currentState);
+        _currentState.Clear();
+    }
+
+    public void SetPosition(Vector2 newPosition) {
+        _currentState.Position = newPosition;
+    }
+
+    public  void SetButtonState(Button button, bool isDown) {
+        _currentState.SetButtonState(button, isDown);
+    }
+
+    public  void AddScrollDelta(Vector2 delta) {
+        _currentState.Scroll += delta;
+    }
+
+    public void AddRelativeDelta(Vector2 delta) {
+        _currentState.RelativeDelta += delta;
+    }
+    
+    internal void Clear() {
+        _currentState = default;
+        _previousState = default;
+    }
+}
---
+++ src/StainedGlass/_Input/MouseState.cs
@@ -0,0 +1,39 @@
+﻿using System.Numerics;
+
+namespace StainedGlass;
+
+public struct MouseState {
+    public Vector2 Position;
+    public Vector2 RelativeDelta;
+    public Vector2 Scroll;
+    private int _buttons;
+
+    public bool IsButtonDown(Mouse.Button button) {
+        return (_buttons & (1 << ((int)button - 1))) != 0;
+    }
+
+    public void SetButtonState(Mouse.Button button, bool isDown) {
+        int buttonBit = 1 << ((int)button - 1);
+        if (isDown) {
+            _buttons |= buttonBit;
+        }
+        else {
+            _buttons &= ~buttonBit;
+        }
+    }
+
+    internal void Clear() {
+        Scroll = Vector2.Zero;
+        RelativeDelta = Vector2.Zero;
+    }
+    
+    internal void ClearAllButtons() {
+        _buttons = 0;
+    }
+    
+    internal void CopyFrom(MouseState source) {
+        Position = source.Position;
+        Scroll = source.Scroll;
+        _buttons = source._buttons;
+    }
+}
